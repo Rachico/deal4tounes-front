b7418e1bc48e4d2cc7f85333334b21b6d8db241a
eyaab <abdelmoulaeya@yahoo.fr>
2020-06-29 12:58:40 +0100

    The page of articles is Ready

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,17 +4,10 @@ import 'tachyons' ;
 function SearchBox({searchfield, searchChange}) {
     return (
       
-<<<<<<< HEAD
                 <div className='pa4 '>
             <input 
             
             className='pa3 ba b--green '
-=======
-        <div className='pa4 '>
-            <input 
-            className='pa3 ba b--green '
-              
->>>>>>> dev
                 type='search' 
                 placeholder='rechercher ici '
                 onChange ={searchChange}
